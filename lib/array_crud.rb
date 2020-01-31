e82108fbb821b1c15073c651f9ed749066dde541
Sultan Afsheen <leadsultan8x@gmail.com>
2020-01-31 01:20:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,8 +2,8 @@ def create_an_empty_array
   []
 end
 
-def create_an_array
-  example_array = ["Sultan", "Mark", "Archi", "Jerome"]
+def create_an_array ["Sultan", "Mark", "Archi", "Jerome"]
+
 end
 
 def add_element_to_end_of_array(array, element)
